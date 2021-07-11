ccad0fc03c76c3069606cae07a9b817b88c505a5
Nikola <nikola.novinc1@gmail.com>
2021-07-11 07:26:07 +0200

    dorade

--- BlazorProjectVenkat/Server/Controllers/EmployeesController.cs
+++ BlazorProjectVenkat/Server/Controllers/EmployeesController.cs
@@ -1,4 +1,6 @@
 ﻿using BlazorProjectVenkat.Server.Models;
+using BlazorProjectVenkat.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,7 +23,60 @@ namespace BlazorProjectVenkat.Server.Controllers
         [HttpGet]
         public async Task<ActionResult> GetEmployees()
         {
-            return Ok(await employeeRepository.GetEmployees());
+
+            try
+            {
+                return Ok(await employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retriveing data from the database");
+            }
+            
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+
+            try
+            {
+                var result = await employeeRepository.GetEmployee(id);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retriveing data from the database");
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+
+            try
+            {
+                if (employee == null) return BadRequest();
+
+                var createdEmployee = await employeeRepository.AddEmployee(employee);
+
+                return CreatedAtAction(nameof(GetEmployee), new { id = createdEmployee.EmployeeId }, createdEmployee );
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creating new employee record");
+            }
+
         }
     }
 }
